ce102d3d083e6029e37c33324ef7c4ee335500da
zprog-workstation <workstation@noreply.com>
2021-03-01 17:41:40 +0100

    Cameras map added to settingsmixin class

--- include/settingsmixin.hpp
+++ include/settingsmixin.hpp
@@ -2,23 +2,28 @@
 
 #include "pch.h"
 
+#include "camera.hpp"
+
 class SettingsMixin
 {
 public:
 
-    SettingsMixin(const std::string file = "./settings.ini");
-    ~SettingsMixin();
+  SettingsMixin(const std::string file = "./settings.ini");
+  ~SettingsMixin();
 
-    void displayContent();
+  void displayContent();
 
-    std::map<
-        std::string,                        // first
-        std::map<std::string, std::string>> // second
-        m_kV;                               // var name
+  std::map<
+    std::string,                        // first
+    std::map<std::string, std::string>> // second
+  m_kV;                               // var name
 
 
 protected:
 
-    std::string m_fileContent;
+  std::string m_fileContent;
+  
+  // Map : name => cameraInstance
+  std::map<std::string, Camera> m_cameras;
     
 };
